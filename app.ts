94d3e84adf4cd5664c49d8ec5f3a6bb88cf76145
danielleit241 <danielleee241@gmail.com>
2025-06-21 09:20:40 +0700

    refactor: reorganize code structure and add literals and enum examples

--- app.ts
+++ app.ts
@@ -1,5 +1,28 @@
-function test(x: number): number {
-  return x * 2;
+//Literals
+let direction: "north" | "south" | "east" | "west";
+direction = "north";
+
+console.log(direction);
+if(direction === "north") {
+    console.log("Heading north!");
 }
 
-console.log(test(5));
+let responseCode: 200 | 404 | 500;
+responseCode = 200;
+console.log(`Response code is: ${responseCode}`);
+
+//Enum
+enum Size{
+    Smallest,
+    Medium,
+    Large,
+}
+var mySize: Size = Size.Medium;
+
+console.log(Size.Smallest);
+console.log(Size.Medium);
+console.log(Size.Large);
+
+if(mySize === Size.Medium) {
+    console.log("My size is medium.");
+}
